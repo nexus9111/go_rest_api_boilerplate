857cb77f4698b17d7ae742b56081e59dd62b5c54
J.Nexus <contactpro.joss@gmail.com>
2023-11-03 11:41:58 +0100

    ci(docker): add docker file

--- users/middlewares.go
+++ users/middlewares.go
@@ -1,31 +1,15 @@
 package users
 
 import (
-	"GO_API_BOILERPLATE/utils"
+	"GO_API_BOILERPLATE/users/shared"
 	"github.com/gin-gonic/gin"
 )
 
 func IsAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var user User
-		user.get(c)
+		var user shared.User
+		user.Get(c)
 		c.Set("user", user)
 		c.Next()
 	}
-}
-
-func (u *User) get(c *gin.Context) {
-	authorization := c.Request.Header["Authorization"]
-	if len(authorization) == 0 {
-		utils.ErrorResponse(c, 401, utils.ErrorUnauthorized)
-		c.Abort()
-	}
-
-	// TODO: check token
-	// TODO: get user from database
-
-	u.Id = "1"
-	u.Email = "test@test.com"
-	u.Username = "test"
-	u.Password = "test"
 }
--- users/shared/models.go
+++ users/shared/models.go
@@ -1,5 +1,10 @@
 package shared
 
+import (
+	"GO_API_BOILERPLATE/utils"
+	"github.com/gin-gonic/gin"
+)
+
 type User struct {
 	Id       string
 	Email    string
@@ -9,4 +14,20 @@ type User struct {
 
 func (u *User) Validate() bool {
 	return u.Email != "" && u.Username != "" && u.Password != ""
+}
+
+func (u *User) Get(c *gin.Context) {
+	authorization := c.Request.Header["Authorization"]
+	if len(authorization) == 0 {
+		utils.ErrorResponse(c, 401, utils.ErrorUnauthorized)
+		c.Abort()
+	}
+
+	// TODO: check token
+	// TODO: get user from database
+
+	u.Id = "1"
+	u.Email = "test@test.com"
+	u.Username = "test"
+	u.Password = "test"
 }
